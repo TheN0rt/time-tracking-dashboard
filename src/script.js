bacc7c75a5372a225212a7b30e5e9641a0cf2329
TheN0rt <89389482+TheN0rt@users.noreply.github.com>
2022-10-04 01:00:28 +0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,47 +1,32 @@
 const timeNow = document.querySelectorAll('.time__now')
 const timePrev = document.querySelectorAll('.time__prev')
 
-fetch("./data.json")
+const setValue = (json, liText) => {
+   for(index in json){
+      timeNow[index].textContent = 
+      json[index].timeframes[liText].current == 1 ? 
+      json[index].timeframes[liText].current + 'hr' : json[index].timeframes[liText].current + 'hrs'
+
+      timePrev[index].textContent = 
+      json[index].timeframes.weekly.previous == 1 ? 'Previous - ' + json[index].timeframes.weekly.previous + 'hr' : 'Previous - ' + json[index].timeframes.weekly.previous + 'hrs'
+   }
+}
+const getValue = async () => {
+   await fetch("./data.json")
    .then(res => res.json())
    .then(json => {
       document.querySelectorAll('.time__variants ul li').forEach(li => {
          li.onclick = () => {
-            
-            if(li.textContent === 'Weekly') {
-               
-               for(index in json){
-                  timeNow[index].textContent = 
-                  json[index].timeframes.weekly.current == 1 ? 
-                  json[index].timeframes.weekly.current + 'hr' : json[index].timeframes.weekly.current + 'hrs'
-
-                  timePrev[index].textContent = 
-                  json[index].timeframes.weekly.previous == 1 ? 'Previous - ' + json[index].timeframes.weekly.previous + 'hr' : 'Previous - ' + json[index].timeframes.weekly.previous + 'hrs'
-               }
-            } 
-
-            if(li.textContent === 'Daily') {
-               for(index in json){
-                  timeNow[index].textContent =
-                  json[index].timeframes.daily.current == 1 ?  json[index].timeframes.daily.current + 'hr' : json[index].timeframes.daily.current + 'hrs'
-                  
-                  timePrev[index].textContent = 
-                  json[index].timeframes.daily.previous == 1 ? 'Previous - ' + json[index].timeframes.daily.previous + 'hr' : 'Previous - ' + json[index].timeframes.daily.previous + 'hrs'
-                   
-               }
-            } 
-
-            if(li.textContent === 'Monthly') {
-               for(index in json){
-                  timeNow[index].textContent = 
-                  json[index].timeframes.monthly.current == 1 ? json[index].timeframes.monthly.current + 'hr' : json[index].timeframes.monthly.current + 'hrs'
-
-                  timePrev[index].textContent = 
-                  json[index].timeframes.monthly.previous == 1 ? 
-                  'Previous - ' + json[index].timeframes.monthly.previous + 'hr' : 'Previous - ' + json[index].timeframes.monthly.previous + 'hrs'
-               }
-            } 
+            let liText = li.textContent.toLowerCase()
+            setValue(json, liText)        
          }
       })
    })
+}
+
+getValue()
+
+      
+
 
 
